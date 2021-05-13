e6ba69095d8585047122775ac813233493936406
Douglas Panhota <douglaspands@gmail.com>
2021-05-13 20:16:34 -0300

    inclusao de api em go

--- go-rest/main.go
+++ go-rest/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
+    "strconv"
+    "encoding/json"
+    "github.com/gorilla/mux"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+type Fibonacci struct {
+	Value int `json:"value,omitempty"`
 }
 
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+type Result struct {
+	Data Fibonacci `json:"data"`
+}
+
+func FibonacciSequence(w http.ResponseWriter, r *http.Request){
+    params := mux.Vars(r)
+    order, err := strconv.Atoi(params["order"])
+    _ = err
+    var f []int
+    f = append(f, 0)
+    f = append(f, 1)
+    for i := 2; i <= order; i++ {
+        f = append(f, f[i - 1] + f[i - 2]);
+    }
+    fibonacci := Fibonacci{Value: f[order]}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(Result{Data: fibonacci})
 }
 
 func main() {
-    handleRequests()
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/fibonacci/v1/sequence/{order:[0-9]+}", FibonacciSequence).Methods("GET")
+    log.Println("App running -> http://localhost:3000")
+    log.Fatal(http.ListenAndServe(":3000", router))
 }
